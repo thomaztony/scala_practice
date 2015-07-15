e4cce13362a8cf4bc8ec160e17c9c9dddbc3b41e
thomaztony <thomaztony@gmail.com>
2015-07-15 19:53:26 +0530

    Commit

--- scala01/src/com/examples/scala/ArraysConcatenate.scala
+++ scala01/src/com/examples/scala/ArraysConcatenate.scala
@@ -7,9 +7,15 @@ object ArraysConcatenate {
 		var myList2: Array[Double] = Array(4.9,5.9,6.9)
 
 		var myList3 = concat (myList1, myList2)
+		var myList4 = Array.concat(myList1,myList2)
 		
 		//printing elements in myList3
 		for (x <- myList3)
 		  println(x)
+		  
+		  println(" separator ")
+		  
+		for (y <- myList4)
+		  println(y)
 	}
 }
