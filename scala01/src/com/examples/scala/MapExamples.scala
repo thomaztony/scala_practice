9e1e7186e8208aab3e03a83c0361ae6fa15b78e0
thomaztony <thomaztony@gmail.com>
2015-07-15 20:31:18 +0530

    Commit

---
+++ scala01/src/com/examples/scala/MapExamples.scala
@@ -0,0 +1,33 @@
+package com.examples.scala
+
+object MapExamples {
+  def main(args: Array[String]): Unit = {
+    val colors1: Map[String, String] = Map("red" -> "#FFGH",
+                     "green" -> "#FGGJ",
+                     "blue" -> "#GHIK")
+    val nums: Map[Int, Int] = Map()
+    
+    println("Keys in colors : "+colors1.keys)
+    println("Values in colors : "+ colors1.values)
+    
+    println("Check if nums is empty : "+ nums.isEmpty)
+    
+    //use the ++ operator or Map.++() to concatenate two or more Maps. It wil remove duplicates while concatenating maps
+    
+    val colors2: Map[String, String] = Map("violet" -> "#BNMS",
+    									   "purple" -> "#VSGH",
+    									   "red" -> "#TYUI")
+    									   
+    val colors = colors1 ++ colors2
+    println("colors1 ++ colors2 : " + colors)
+    
+    //printing keys and values from a map
+    
+    colors.keys.foreach{ i =>
+      print("Key = " + i)
+      println (" Value = "+colors(i))
+    }
+    
+  }
+
+}
